d9495230ab531adad1e310602275da7cbba97c47
Norman <normanyu@NLP-L-NYU1.local>
2013-12-22 13:16:28 -0500

    Signups working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(new_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private 
+  def new_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
