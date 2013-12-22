efa10f61e4d59daa042b0733657aa859a952824a
Norman <normanyu@NLP-L-NYU1.local>
2013-12-22 10:04:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dinami::Application.config.secret_key_base = '10eeca191a4355fe391762f58f34dabc8b09246b404773f58955e2ee69661275b16474b7338f9cbfb2cf4c213ebfc7c2ba04dd5d09e96b09b2ba6387e747262f'
