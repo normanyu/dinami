889971e577d11385eeac05f1224272d8ad5355ef
Norman <normanyu@NLP-L-NYU1.local>
2013-12-23 20:05:50 -0500

    Dinami Customization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Dinami!"
       redirect_to @user
     else
       render 'new'
