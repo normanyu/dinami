87622e1a6b0ac20e5e71546a2f43d79c96d1134f
Norman <normanyu@NLP-L-NYU1.local>
2013-12-23 19:08:19 -0500

    Finished all 9.2 tests. Authorizations in place

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -20,12 +21,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-     @user = User.find(params[:id])
-   end
+  end
    
    
    def update
-     @user = User.find(params[:id])
      if @user.update_attributes(user_params)
        flash[:success] = "Profile updated"
        redirect_to @user     
@@ -41,7 +40,14 @@ class UsersController < ApplicationController
   end
   
   def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
-  
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
