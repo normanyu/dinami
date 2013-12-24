ddc94161edc800aa12cd8aac16591034a8432870
Norman <normanyu@NLP-L-NYU1.local>
2013-12-23 22:57:17 -0500

    Finished formatting and displaying microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @feedbacks = User.find(params[:id]).feedbacks.paginate(page: params[:page])
   end
 
 
